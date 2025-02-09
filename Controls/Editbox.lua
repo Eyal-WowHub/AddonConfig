b348eb9ca24abdf8f516589f2d23d4281f2baa36
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-02-09 17:08:37 +0200

    Refactor Editbox control to prevent breakage and improve encapsulation
    Refactored the Editbox control to ensure functionality remains intact in the event of future Blizzard API changes. This update enhances stability and protects against potential issues from unanticipated API updates.

--- Controls/Editbox.lua
+++ Controls/Editbox.lua
@@ -14,7 +14,7 @@ local function Constructor(template)
 
     template.default = template.default or ""
     template.options = template.options or {}
-    template.options.text = template.options.text or template.name
+    template.options.label = template.options.label or template.name
     template.options.validate = template.validate
     
     template:InitializeControl("AddonConfigEditboxControlTemplate")
--- Controls/Templates/Editbox.lua
+++ Controls/Templates/Editbox.lua
@@ -1,91 +1,97 @@
-AddonConfigEditboxControlMixin = CreateFromMixins(SettingsControlMixin)
+--[[ Control API ]]
 
---[[ Control ]]
-
-local function HideButton(self)
-    self.Button:Hide()
-    self.Editbox:SetTextInsets(0, 0, 3, 3)
-end
-
-local function ShowButton(self)
-    if not self.__disabledButton then
-        self.Button:Show()
-        self.Editbox:SetTextInsets(0, 20, 3, 3)
-    else
-        HideButton(self)
-    end
-end
+local Control = {}
 
 local function OnEnterPressed(self)
     PlaySound(856) -- SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON
     self.Editbox:ClearFocus()
-    HideButton(self)
+    self:HideButton()
 end
 
 local function OnEscapePressed(self)
     self.Editbox:ClearFocus()
 end
 
-local function SetText(self, value)
-    self.__lastText = value or ""
-    self.Editbox:SetText(value or "")
-    self.Editbox:SetCursorPosition(0)
-    HideButton(self)
-end
-
 local function OnTextChanged(self)
     local text = self.Editbox:GetText()
-    if tostring(text) ~= tostring(self.__lastText) then
-        if self.__validate and type(self.__validate) == "function" then
-            if not self.__validate(text) then
-                SetText(self, self.__lastText)
+    if tostring(text) ~= tostring(self.lastText) then
+        if self.validate and type(self.validate) == "function" then
+            if not self.validate(text) then
+                self:SetText(self.lastText)
                 return
             end
         end
-        self.__lastText = text
-        self:GetSetting():SetValue(text)
-        ShowButton(self)
+        self.lastText = text
+        self.Setting:SetValue(text)
+        self:ShowButton()
     end
 end
 
-local function SetMaxLetters(self, value)
+function Control:HideButton()
+    self.Button:Hide()
+    self.Editbox:SetTextInsets(0, 0, 3, 3)
+end
+
+function Control:ShowButton()
+    if not self.__disabledButton then
+        self.Button:Show()
+        self.Editbox:SetTextInsets(0, 20, 3, 3)
+    else
+        self:HideButton()
+    end
+end
+
+function Control:SetText(value)
+    self.lastText = value or ""
+    self.Editbox:SetText(value or "")
+    self.Editbox:SetCursorPosition(0)
+    self:HideButton()
+end
+
+function Control:SetMaxLetters(value)
     self.Editbox:SetMaxLetters(value or 0)
 end
 
-local function Disable(self)
-    SetText(self)
+function Control:Disable()
+    self:SetText()
     self.Editbox:EnableMouse(false)
     self.Editbox:ClearFocus()
     self.Editbox:SetTextColor(0.5, 0.5, 0.5)
     self:DisplayEnabled(false)
 end
 
-local function Enable(self)
+function Control:Enable()
     self.Editbox:EnableMouse(true)
     self.Editbox:SetTextColor(1, 1, 1)
     self:DisplayEnabled(true)
 end
 
-local function SetLabel(self, value)
+function Control:SetLabel(value)
     if value and value ~= "" then
-        self.Text:SetText(value)
-        self.Text:Show()
+        self.Label:SetText(value)
+        self.Label:Show()
     else
-        self.Text:SetText("")
-        self.Text:Hide()
+        self.Label:SetText("")
+        self.Label:Hide()
     end
 end
 
-local function Default(self)
-    SetText(self)
-    Enable(self)
-    SetMaxLetters(self)
+function Control:Default()
+    self:SetText()
+    self:Enable()
+    self:SetMaxLetters()
 end
 
---[[ Bootstrapping ]]
+--[[ Control Implementation ]]
+
+AddonConfigEditboxControlMixin = CreateFromMixins(SettingsControlMixin)
 
 function AddonConfigEditboxControlMixin:OnLoad()
     SettingsControlMixin.OnLoad(self)
+
+    self.Control = setmetatable({}, { __index = Control })
+    self.Control.Setting = self:GetSetting()
+    self.Control.Label = self.Text
 
     self.Tooltip:ClearAllPoints()
     self.Tooltip:Hide()
@@ -94,51 +100,51 @@ function AddonConfigEditboxControlMixin:OnLoad()
     self.Tooltip.HoverBackground:Hide()
 
     local editbox = CreateFrame("EditBox", nil, self, "InputBoxTemplate")
-    self.Editbox = editbox
+    self.Control.Editbox = editbox
     editbox:SetAutoFocus(false)
     editbox:SetTextInsets(0, 0, 3, 3)
     editbox:SetMaxLetters(256)
     editbox:SetPoint("CENTER", self, "CENTER", 27, 0)
     editbox:SetWidth(200)
     editbox:SetHeight(44)
-    editbox:SetScript("OnTextChanged", function(_) OnTextChanged(self) end)
-    editbox:SetScript("OnEnterPressed", function(_) OnEnterPressed(self) end)
-    editbox:SetScript("OnEscapePressed", function(_) OnEscapePressed(self) end)
+    editbox:SetScript("OnTextChanged", function(_) OnTextChanged(self.Control) end)
+    editbox:SetScript("OnEnterPressed", function(_) OnEnterPressed(self.Control) end)
+    editbox:SetScript("OnEscapePressed", function(_) OnEscapePressed(self.Control) end)
 
     local button = CreateFrame("Button", nil, editbox, "UIPanelButtonTemplate")
-    self.Button = button
+    self.Control.Button = button
     button:SetWidth(40)
     button:SetHeight(20)
     button:SetPoint("RIGHT", -2, 0)
     button:SetText(OKAY)
     button:Hide()
-    button:SetScript("OnClick", function(_) OnEnterPressed(self) end)
+    button:SetScript("OnClick", function(_) OnEnterPressed(self.Control) end)
 
-    Default(self)
+    self.Control:Default()
 end
 
 function AddonConfigEditboxControlMixin:Init(initializer)
     SettingsControlMixin.Init(self, initializer)
 
-    local setting = self:GetSetting()
+    local control = self.Control
     local options = initializer:GetOptions()
-
-    SetText(self, setting:GetValue())
+    
+    control:SetText(control.Setting:GetValue())
 
     if options then
-        SetMaxLetters(self, options.maxLetters)
-        SetLabel(self, options.text)
+        control:SetMaxLetters(options.maxLetters)
+        control:SetLabel(options.label)
 
-        self.__validate = options.validate
+        control.validate = options.validate
         
         if options.disabled and type(options.disabled) == "function" then
             local function onValueChanged()
                 local disabled = options.disabled()
 
                 if disabled then
-                    Disable(self)
+                    control:Disable()
                 else
-                    Enable(self)
+                    control:Enable()
                 end
             end
         
@@ -154,9 +160,12 @@ function AddonConfigEditboxControlMixin:OnSettingValueChanged(setting, value)
 end
 
 function AddonConfigEditboxControlMixin:Release()
-    self.Editbox:SetScript("OnEnterPressed", nil)
-    self.Editbox:SetScript("OnEscapePressed", nil)
-    self.Editbox:SetScript("OnTextChanged", nil)
-    self.Button:SetScript("OnClick", nil)
+    local control = self.Control
+
+    control.Editbox:SetScript("OnEnterPressed", nil)
+    control.Editbox:SetScript("OnEscapePressed", nil)
+    control.Editbox:SetScript("OnTextChanged", nil)
+    control.Button:SetScript("OnClick", nil)
+
     SettingsControlMixin.Release(self)
 end
