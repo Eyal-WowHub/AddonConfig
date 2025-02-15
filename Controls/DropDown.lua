8ce037645a282a87c8626f65be7e714580fca694
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2025-02-15 17:34:22 +0200

    Update DropDown control to set the variable type correctly

--- Controls/DropDown.lua
+++ Controls/DropDown.lua
@@ -3,7 +3,6 @@ local lib = LibStub and LibStub("AddonConfig-1.0", true)
 if not lib or lib:GetControlVersion(Name) >= Version then return end
 
 local Schema = {
-    default = "number",
     get = "function",
     set = "function",
     tooltip = "string?",
@@ -12,14 +11,21 @@ local Schema = {
 
 local function GetOptions(template)
     local container = Settings.CreateControlTextContainer()
+    local value = template.default
 
     for index, option in ipairs(template.options) do
         local optionType = type(option)
         if optionType == "table" then
-            container:Add(index, option.text, option.tooltip)
-        elseif optionType == "string" then
+            value = value or option.value or index
+            container:Add(option.value or index, option.text, option.tooltip)
+        elseif optionType == "string" or optionType == "number" or optionType == "boolean" then
+            value = value or index
             container:Add(index, option)
         end
+    end
+
+    if not template:HasVariableType() then
+        template:SetVariableTypeByValue(value)
     end
 
     return container:GetData()
@@ -29,9 +35,10 @@ local function Constructor(template)
     template:Validate(Schema)
 
     local parent = template:GetParentInfo()
+    local options = GetOptions(template)
     local setting = template:RegisterControlSetting()
 
-	Settings.CreateDropdown(parent.category, setting, GetOptions(template), template.tooltip)
+	Settings.CreateDropdown(parent.category, setting, options, template.tooltip)
 end
 
 lib:RegisterControl(Name, Version, Constructor)
